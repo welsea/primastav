2dde5141d93c4bcc2356d45fe143b6bde2357c1b
welsea <welsea.42@gmail.com>
2024-10-15 16:58:48 +0200

    add fading out

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,32 @@
-console.log("hello");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("var table = document.getElementById('info');\nvar runes = document.getElementById('runes');\nvar timeoutID = setTimeout(function () {\n    showinfo();\n}, 500);\nfunction showinfo() {\n    var op = 1;\n    var intervalID = setInterval(function () {\n        if (op > 0.0) {\n            runes.style.opacity = op.toString();\n            op = Number((op - 0.1).toFixed(2));\n            console.log(op);\n        }\n        else {\n            runes.classList.add('hide');\n            clearInterval(intervalID);\n        }\n    }, 200);\n    var op2 = 0.0;\n    setTimeout(function () {\n        table.classList.remove('hide');\n        table.style.opacity = '0';\n        var intervalID2 = setInterval(function () {\n            if (op2 <= 1.0) {\n                table.style.opacity = op2.toString();\n                op2 = op2 + 0.1;\n            }\n            else {\n                clearInterval(intervalID2);\n            }\n        }, 200);\n    }, 1000);\n}\n\n\n//# sourceURL=webpack://primstav/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-// style.backgroundImage = "url('img_tree.png')"; 
+const table = document.getElementById('info');
+const runes = document.getElementById('runes');
+const timeoutID = setTimeout(() => {
+    showinfo()
+}, 500);
+function showinfo() {
+    let op: number = 1
+    const intervalID = setInterval(() => {
+        if (op > 0.0) {
+            runes.style.opacity = op.toString()
+            op = Number((op - 0.1).toFixed(2))
+            console.log(op)
+        } else {
+            runes.classList.add('hide')
+            clearInterval(intervalID)
+        }
+    }, 200);
 
+    let op2 = 0.0
+    setTimeout(() => {
+        table.classList.remove('hide')
+        table.style.opacity = '0'
+        const intervalID2 = setInterval(() => {
+            if (op2 <= 1.0) {
+                table.style.opacity = op2.toString()
+                op2 = op2 + 0.1
+            } else {
+                clearInterval(intervalID2)
+            }
+        }, 200);
+    }, 1000);
+
+
+}
