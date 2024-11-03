43546b7cc682990d1c504e8d7727f6a8c47f5d38
welsea <welsea.42@gmail.com>
2024-11-03 12:27:54 +0100

    update layout

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
-const table = document.getElementById('info');
-const runes = document.getElementById('runes');
-const timeoutID = setTimeout(() => {
-    showinfo()
-}, 500);
-function showinfo() {
-    let op: number = 1
+function main() {
+  const table = document.getElementById("info");
+  const runes = document.getElementById("runes");
+  const timeoutID = setTimeout(() => {
+    showinfo();
+  }, 500);
+  function showinfo() {
+    let op: number = 1;
     const intervalID = setInterval(() => {
-        if (op > 0.0) {
-            runes.style.opacity = op.toString()
-            op = Number((op - 0.1).toFixed(2))
-            console.log(op)
-        } else {
-            runes.classList.add('hide')
-            clearInterval(intervalID)
-        }
+      if (op > 0.0) {
+        runes.style.opacity = op.toString();
+        op = Number((op - 0.1).toFixed(2));
+        console.log(op);
+      } else {
+        clearInterval(intervalID);
+      }
     }, 200);
 
-    let op2 = 0.0
+    let op2 = 0.0;
     setTimeout(() => {
-        table.classList.remove('hide')
-        table.style.opacity = '0'
-        const intervalID2 = setInterval(() => {
-            if (op2 <= 1.0) {
-                table.style.opacity = op2.toString()
-                op2 = op2 + 0.1
-            } else {
-                clearInterval(intervalID2)
-            }
-        }, 200);
+      table.classList.remove("hide");
+      table.style.opacity = "0";
+      const intervalID2 = setInterval(() => {
+        if (op2 <= 1.0) {
+          table.style.opacity = op2.toString();
+          op2 = op2 + 0.1;
+        } else {
+          clearInterval(intervalID2);
+        }
+      }, 200);
     }, 1000);
-
-
+  }
 }
+
+
+main()
